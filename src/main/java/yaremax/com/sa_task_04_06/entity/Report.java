abb56bc5ddb1dc8825660b4400d7fad19e5315a5
Maksym Yarema <macsym2@gmail.com>
2024-06-09 11:57:04 +0300

    feat(Report): override equals and hashcode

--- src/main/java/yaremax/com/sa_task_04_06/entity/Report.java
+++ src/main/java/yaremax/com/sa_task_04_06/entity/Report.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
@@ -28,4 +29,17 @@ public class Report {
     private LocalDate reportDate;
     private BigDecimal totalRevenue;
     private BigDecimal netProfit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return Objects.equals(id, report.id) && Objects.equals(company, report.company) && Objects.equals(reportDate, report.reportDate) && Objects.equals(totalRevenue, report.totalRevenue) && Objects.equals(netProfit, report.netProfit);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, company, reportDate, totalRevenue, netProfit);
+    }
 }
