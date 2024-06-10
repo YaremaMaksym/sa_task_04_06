2e3f5e72529b98455d19db37a7f3122f619bf08b
Maksym Yarema <macsym2@gmail.com>
2024-06-10 10:07:26 +0300

    fix: SaTask0406ApplicationTests, @ActiveProfiles("test")

--- src/test/java/yaremax/com/sa_task_04_06/SaTask0406ApplicationTests.java
+++ src/test/java/yaremax/com/sa_task_04_06/SaTask0406ApplicationTests.java
@@ -2,8 +2,10 @@ package yaremax.com.sa_task_04_06;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(classes = SaTask0406Application.class)
 class SaTask0406ApplicationTests {
 
     @Test
